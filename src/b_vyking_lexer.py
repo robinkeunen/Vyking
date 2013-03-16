ef9ee64f284ee3a389a3d0697b7a1675fa02df60
Robin Keunen <robin@keunen.net>
2013-03-16 18:08:50 +0100

    basic types token

--- src/b_vyking_lexer.py
+++ src/b_vyking_lexer.py
@@ -1,23 +1,51 @@
 __author__ = 'Robin Keunen'
 __author__ = 'Pierre Vyncke'
 
-import ply.lex
+import ply.lex as lex
+import test_units
+from ply.lex import TOKEN
+
+# basic regex
+number = r'([\+-][1-9]\d*)|0'
+exponent = r'(e|E)' + number
 
 # Token definitions
 # Token defined as functions when an action is needed
+# Evaluation order :
+#  functions in order of definition
+#  definitions in decreasing regex length
 t_PLUS   = r'\+'
 t_MINUS  = r'-'
 t_TIMES  = r'\*'
 t_DIVIDE = r'/'
+t_ASSIGN = r'='
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_COLON  = r'\:'
 t_NEWLINE = r'\n'
+t_EQ = r'=='
+t_LOWER = r'<'
+t_GREATER = r'>'
+t_LEQ = r'<='
+t_GEQ = r'>='
+t_NEQ = r'!='
 
 
-def t_NUMBER(t):
-    r'\d+'
+@TOKEN(number + r'\.\d*' + exponent + r'?')
+def t_FLOAT(t):
+    t.value = float(t.value)
+    return t
+
+
+@TOKEN(number)
+def t_INT(t):
     t.value = int(t.value)
+    return t
+
+
+def t_BOOLEAN(t):
+    r'(True | False)'
+    t.value = bool(t.value)
     return t
 
 
@@ -33,6 +61,7 @@ def t_COMMENT(t):
     pass
     # No return value. Token discarded
 
+t_ignore_whitespace = r'\s'
 
 # Track line number
 def t_newline(t):
@@ -54,16 +83,24 @@ reserved = {
 }
 
 # Token list
-tokens = ['NUMBER',
-          'ID',
+tokens = ['ID',
+          'INT',
+          'FLOAT',
           'PLUS',
           'MINUS',
           'TIMES',
           'DIVIDE',
+          'ASSIGN',
           'LPAREN',
           'RPAREN',
           'COLON',
-          'NEWLINE'] + list(reserved.values())
+          'NEWLINE',
+          'EQ',
+          'LOWER',
+          'GREATER',
+          'LEQ',
+          'GEQ',
+          'NEQ'] + list(reserved.values())
 
 # Compute column.
 #    input is the input text string
@@ -74,3 +111,15 @@ def find_column(input, token):
         last_cr = 0
     column = (token.lexpos - last_cr) + 1
     return column
+
+# Error handling rule
+# Print offending character and skip
+def t_error(t):
+    print "Illegal character '%s'" % t.value[0]
+    t.lexer.skip(1)
+
+lexer = lex.lex()
+lex.input(test_units.exp1)
+
+for tok in lexer:
+    print tok
---
+++ src/test_units.py
@@ -0,0 +1,3 @@
+__author__ = 'Robin Keunen'
+
+exp1 = "x = 3 + 42 * (s - t)"
